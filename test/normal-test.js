8451d6af009c935bea0fc0174691edab13da9a2f
Mike Bostock <mbostock@gmail.com>
2015-06-09 09:48:17 -0700

    Delete Kolmogorov-Smirnov test.
    With apologies to @dangoldbach, but this test didn’t appear to actually be
    verifying that the random number generators passed the KS test: for example,
    replacing random.normal with random.uniform still passed. So I’m going to
    replace this test method with a few simpler tests until I have time to learn how
    KS tests should work.

--- test/normal-test.js
+++ test/normal-test.js
@@ -2,25 +2,10 @@ var tape = require("tape"),
     random = require("../");
 
 require("seedrandom");
-require("./kolmogorovSmirnov");
 
 var mathRandom = Math.random;
-
-tape("normal(mu, sigma) returns random numbers with a normal distribution", function(test) {
-  Math.seedrandom("a random seed.");
-  var mu = -43289, sigma = 38.8;
-  test.kolmogorovSmirnov(random.uniform(mu, sigma), normalCdf(mu, sigma));
-  test.end();
-});
 
 tape("normal() [teardown]", function(test) {
   Math.random = mathRandom;
   test.end();
 });
-
-// Logistic approximation to normal CDF around N(mu, sigma).
-function normalCdf(mu, sigma) {
-  return function(x) {
-    return 1 / (1 + Math.exp(-0.07056 * Math.pow((x - mu)/ sigma, 3) - 1.5976 * (x - mu)/ sigma));
-  };
-}
