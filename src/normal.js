69b022d2fe7d10e7f253cadb727a2b0318cb9fc0
Mike Bostock <mbostock@gmail.com>
2016-01-12 12:32:52 -0800

    Fix #3 - use previously-generated random in Box–Muller.

--- src/normal.js
+++ src/normal.js
@@ -1,15 +1,21 @@
 export default function(mu, sigma) {
-  var n = arguments.length;
+  var n = arguments.length, x, r;
   if (!n) mu = 0, sigma = 1;
   else if (n === 1) mu = +mu, sigma = 1;
   else mu = +mu, sigma = +sigma;
   return function() {
-    var x, y, r;
-    do {
+    var y;
+
+    // If available, use the second previously-generated uniform random.
+    if (x != null) y = x, x = null;
+
+    // Otherwise, generate a new x and y.
+    else do {
       x = Math.random() * 2 - 1;
       y = Math.random() * 2 - 1;
       r = x * x + y * y;
     } while (!r || r > 1);
-    return mu + sigma * x * Math.sqrt(-2 * Math.log(r) / r);
+
+    return mu + sigma * y * Math.sqrt(-2 * Math.log(r) / r);
   };
 };
