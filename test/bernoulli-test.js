463d5f893adf4ad8b07587b79a2c8de0c4592d7d
Pieter <pietervanderveeken@gmail.com>
2016-03-06 00:24:58 +0100

    Added more throrough tests

--- test/bernoulli-test.js
+++ test/bernoulli-test.js
@@ -6,36 +6,36 @@ var tape = require("tape"),
 
 require("seedrandom");
 require("./inDelta");
+require("./skewness");
+require("./kurtosis");
 
 var mathRandom = Math.random;
+var mean = function(p) { return p};
+var variance = function(p) { return p * (1 - p)};
+var skew = function(p) { return (1 - 2 * p) / Math.sqrt(variance(p))};
 
-tape.test("randomBernoulli(p) returns random bernoulli distributed numbers with a mean of .5 and a variance of .25.", function(test) {
+tape.test("randomBernoulli(p) returns random bernoulli distributed numbers with a mean of p", function(test) {
 	Math.seedrandom("d5cb594f444fc692");
-
-	var p = 0.5;
-	var nums = array.range(10000).map(random.randomBernoulli(p));
-
-	test.inDelta(array.mean(nums), p, p * (1 - p));
+	test.inDelta(array.mean(array.range(10000).map(random.randomBernoulli(1))), mean(1), variance(1));
+	test.inDelta(array.mean(array.range(10000).map(random.randomBernoulli(.5))), mean(.5), variance(.5));
+	test.inDelta(array.mean(array.range(10000).map(random.randomBernoulli(.25))), mean(.25), variance(.25));
+	test.inDelta(array.mean(array.range(10000).map(random.randomBernoulli(0))), mean(0), variance(0));
 	test.end();
 });
 
-tape.test("randomBernoulli(p) returns random bernoulli distributed numbers with a mean of 0 and a variance of 0.", function(test) {
+tape.test("randomBernoulli(p) returns random bernoulli distributed numbers with a variance of p * (1 - p)", function(test) {
 	Math.seedrandom("c4af5ee918417093");
-
-	var p = 0;
-	var nums = array.range(10000).map(random.randomBernoulli(p));
-
-	test.inDelta(array.mean(nums), p, p * (1 - p));
+	test.inDelta(array.variance(array.range(10000).map(random.randomBernoulli(1))), variance(1), 0);
+	test.inDelta(array.variance(array.range(10000).map(random.randomBernoulli(.5))), variance(.5), 0.05);
+	test.inDelta(array.variance(array.range(10000).map(random.randomBernoulli(.25))), variance(.25), 0.05);
+	test.inDelta(array.variance(array.range(10000).map(random.randomBernoulli(0))), variance(0), 0);
 	test.end();
 });
 
-tape.test("randomBernoulli(p) returns random bernoulli distributed numbers with a mean of 1 and a variance of 0.", function(test) {
+tape.test("randomBernoulli(p) returns random bernoulli distributed numbers with a skewness of (1 - 2 * p) / sqrt(p * (1 - p)).", function(test) {
 	Math.seedrandom("bb0bb470f346ff65");
-
-	var p = 1;
-	var nums = array.range(10000).map(random.randomBernoulli(p));
-
-	test.inDelta(array.mean(nums), p, p * (1 - p));
+	test.inDelta(skewness(array.range(10000).map(random.randomBernoulli(.5))), skew(.5), 0.05);
+	test.inDelta(skewness(array.range(10000).map(random.randomBernoulli(.25))), skew(.25), 0.05);
 	test.end();
 });
 
