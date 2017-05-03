3a1f7446d19e8a7282c0b9a7139d24af974b8195
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-05-02 20:43:22 -0400

    updating tests

--- test/exponential-test.js
+++ test/exponential-test.js
@@ -1,18 +1,16 @@
 var tape = require("tape"),
     array = require("d3-array"),
+    seedrandom = require("seedrandom"),
     random = require("../");
 
-require("seedrandom");
 require("./inDelta");
 
-var mathRandom = Math.random;
-
 tape.test("randomExponential(lambda) returns random exponentially distributed numbers with a mean of 1/lambda.", function(test) {
-  Math.seedrandom("d5cb594f444fc692");
+  var randomExponential = random.randomExponential.source(seedrandom("d5cb594f444fc692"));
 
   var mean = 20,
       lambda = 1 / mean, // average rate (e.g. 1 per 20 minutes)
-      times = array.range(10000).map(random.randomExponential(lambda));
+      times = array.range(10000).map(randomExponential(lambda));
 
   test.inDelta(array.mean(times), mean, mean * 0.05);
 
@@ -23,10 +21,5 @@ tape.test("randomExponential(lambda) returns random exponentially distributed nu
     test.inDelta(within.length / times.length, expected, expected * 0.02);
   });
 
-  test.end();
-});
-
-tape("randomExponential() [teardown]", function(test) {
-  Math.random = mathRandom;
   test.end();
 });
