9c443e79804884db2f9cac73a7ef3f6b18a71590
Mike Bostock <mbostock@gmail.com>
2017-05-03 10:19:03 -0700

    Style changes for tests.

--- test/logNormal-test.js
+++ test/logNormal-test.js
@@ -1,41 +1,40 @@
 var tape = require("tape"),
-    array = require("d3-array"),
     seedrandom = require("seedrandom"),
-    random = require("../");
+    d3 = Object.assign({}, require("../"), require("d3-array"));
 
 require("./inDelta");
 
-tape.test("randomLogNormal() returns random numbers with a log-mean of zero", function(test) {
-  var randomLogNormal = random.randomLogNormal.source(seedrandom("a22ebc7c488a3a47"));
-  test.inDelta(array.mean(array.range(10000).map(randomLogNormal()), Math.log), 0, .05);
+tape.test("d3.randomLogNormal() returns random numbers with a log-mean of zero", function(test) {
+  var randomLogNormal = d3.randomLogNormal.source(seedrandom("a22ebc7c488a3a47"));
+  test.inDelta(d3.mean(d3.range(10000).map(randomLogNormal()), Math.log), 0, 0.05);
   test.end();
 });
 
-tape.test("randomLogNormal() returns random numbers with a log-standard deviation of one", function(test) {
-  var randomLogNormal = random.randomLogNormal.source(seedrandom("06fd26b46c25607e"));
-  test.inDelta(array.deviation(array.range(10000).map(randomLogNormal()), Math.log), 1, .05);
+tape.test("d3.randomLogNormal() returns random numbers with a log-standard deviation of one", function(test) {
+  var randomLogNormal = d3.randomLogNormal.source(seedrandom("06fd26b46c25607e"));
+  test.inDelta(d3.deviation(d3.range(10000).map(randomLogNormal()), Math.log), 1, 0.05);
   test.end();
 });
 
-tape.test("randomLogNormal(mu) returns random numbers with the specified log-mean", function(test) {
-  var randomLogNormal = random.randomLogNormal.source(seedrandom("fffe77600db5c1ad"));
-  test.inDelta(array.mean(array.range(10000).map(randomLogNormal(42)), Math.log), 42, .05);
-  test.inDelta(array.mean(array.range(10000).map(randomLogNormal(-2)), Math.log), -2, .05);
+tape.test("d3.randomLogNormal(mu) returns random numbers with the specified log-mean", function(test) {
+  var randomLogNormal = d3.randomLogNormal.source(seedrandom("fffe77600db5c1ad"));
+  test.inDelta(d3.mean(d3.range(10000).map(randomLogNormal(42)), Math.log), 42, 0.05);
+  test.inDelta(d3.mean(d3.range(10000).map(randomLogNormal(-2)), Math.log), -2, 0.05);
   test.end();
 });
 
-tape.test("randomLogNormal(mu) returns random numbers with a log-standard deviation of 1", function(test) {
-  var randomLogNormal = random.randomLogNormal.source(seedrandom("9caf2156de45315a"));
-  test.inDelta(array.deviation(array.range(10000).map(randomLogNormal(42)), Math.log), 1, .05);
-  test.inDelta(array.deviation(array.range(10000).map(randomLogNormal(-2)), Math.log), 1, .05);
+tape.test("d3.randomLogNormal(mu) returns random numbers with a log-standard deviation of 1", function(test) {
+  var randomLogNormal = d3.randomLogNormal.source(seedrandom("9caf2156de45315a"));
+  test.inDelta(d3.deviation(d3.range(10000).map(randomLogNormal(42)), Math.log), 1, 0.05);
+  test.inDelta(d3.deviation(d3.range(10000).map(randomLogNormal(-2)), Math.log), 1, 0.05);
   test.end();
 });
 
-tape.test("randomLogNormal(mu, sigma) returns random numbers with the specified log-mean and log-standard deviation", function(test) {
-  var randomLogNormal = random.randomLogNormal.source(seedrandom("c0d761f591fb5e43"));
-  test.inDelta(array.mean(array.range(10000).map(randomLogNormal(42, 2)), Math.log), 42, .05);
-  test.inDelta(array.mean(array.range(10000).map(randomLogNormal(-2, 2)), Math.log), -2, .05);
-  test.inDelta(array.deviation(array.range(10000).map(randomLogNormal(42, 2)), Math.log), 2, .05);
-  test.inDelta(array.deviation(array.range(10000).map(randomLogNormal(-2, 2)), Math.log), 2, .05);
+tape.test("d3.randomLogNormal(mu, sigma) returns random numbers with the specified log-mean and log-standard deviation", function(test) {
+  var randomLogNormal = d3.randomLogNormal.source(seedrandom("c0d761f591fb5e43"));
+  test.inDelta(d3.mean(d3.range(10000).map(randomLogNormal(42, 2)), Math.log), 42, 0.05);
+  test.inDelta(d3.mean(d3.range(10000).map(randomLogNormal(-2, 2)), Math.log), -2, 0.05);
+  test.inDelta(d3.deviation(d3.range(10000).map(randomLogNormal(42, 2)), Math.log), 2, 0.05);
+  test.inDelta(d3.deviation(d3.range(10000).map(randomLogNormal(-2, 2)), Math.log), 2, 0.05);
   test.end();
 });
--- test/normal-test.js
+++ test/normal-test.js
@@ -1,41 +1,40 @@
 var tape = require("tape"),
-    array = require("d3-array"),
     seedrandom = require("seedrandom"),
-    random = require("../");
+    d3 = Object.assign({}, require("../"), require("d3-array"));
 
 require("./inDelta");
 
 tape.test("randomNormal() returns random numbers with a mean of zero", function(test) {
-  var randomNormal = random.randomNormal.source(seedrandom("a22ebc7c488a3a47"));
-  test.inDelta(array.mean(array.range(10000).map(randomNormal())), 0, .05);
+  var randomNormal = d3.randomNormal.source(seedrandom("a22ebc7c488a3a47"));
+  test.inDelta(d3.mean(d3.range(10000).map(randomNormal())), 0, .05);
   test.end();
 });
 
 tape.test("randomNormal() returns random numbers with a standard deviation of one", function(test) {
-  var randomNormal = random.randomNormal.source(seedrandom("06fd26b46c25607e"));
-  test.inDelta(array.deviation(array.range(10000).map(randomNormal())), 1, .05);
+  var randomNormal = d3.randomNormal.source(seedrandom("06fd26b46c25607e"));
+  test.inDelta(d3.deviation(d3.range(10000).map(randomNormal())), 1, .05);
   test.end();
 });
 
 tape.test("randomNormal(mu) returns random numbers with the specified mean", function(test) {
-  var randomNormal = random.randomNormal.source(seedrandom("fffe77600db5c1ad"));
-  test.inDelta(array.mean(array.range(10000).map(randomNormal(42))), 42, .05);
-  test.inDelta(array.mean(array.range(10000).map(randomNormal(-2))), -2, .05);
+  var randomNormal = d3.randomNormal.source(seedrandom("fffe77600db5c1ad"));
+  test.inDelta(d3.mean(d3.range(10000).map(randomNormal(42))), 42, .05);
+  test.inDelta(d3.mean(d3.range(10000).map(randomNormal(-2))), -2, .05);
   test.end();
 });
 
 tape.test("randomNormal(mu) returns random numbers with a standard deviation of 1", function(test) {
-  var randomNormal = random.randomNormal.source(seedrandom("9caf2156de45315a"));
-  test.inDelta(array.deviation(array.range(10000).map(randomNormal(42))), 1, .05);
-  test.inDelta(array.deviation(array.range(10000).map(randomNormal(-2))), 1, .05);
+  var randomNormal = d3.randomNormal.source(seedrandom("9caf2156de45315a"));
+  test.inDelta(d3.deviation(d3.range(10000).map(randomNormal(42))), 1, .05);
+  test.inDelta(d3.deviation(d3.range(10000).map(randomNormal(-2))), 1, .05);
   test.end();
 });
 
 tape.test("randomNormal(mu, sigma) returns random numbers with the specified mean and standard deviation", function(test) {
-  var randomNormal = random.randomNormal.source(seedrandom("c0d761f591fb5e43"));
-  test.inDelta(array.mean(array.range(10000).map(randomNormal(42, 2))), 42, .05);
-  test.inDelta(array.mean(array.range(10000).map(randomNormal(-2, 2))), -2, .05);
-  test.inDelta(array.deviation(array.range(10000).map(randomNormal(42, 2))), 2, .05);
-  test.inDelta(array.deviation(array.range(10000).map(randomNormal(-2, 2))), 2, .05);
+  var randomNormal = d3.randomNormal.source(seedrandom("c0d761f591fb5e43"));
+  test.inDelta(d3.mean(d3.range(10000).map(randomNormal(42, 2))), 42, .05);
+  test.inDelta(d3.mean(d3.range(10000).map(randomNormal(-2, 2))), -2, .05);
+  test.inDelta(d3.deviation(d3.range(10000).map(randomNormal(42, 2))), 2, .05);
+  test.inDelta(d3.deviation(d3.range(10000).map(randomNormal(-2, 2))), 2, .05);
   test.end();
 });
