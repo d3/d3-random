6b838f6ebbd8be223f24231e06948f6b82b4ceaa
Alex Van Camp <vancamp.alex@gmail.com>
2018-11-11 12:45:58 -0600

    allow loading in browser via es modules
    Browser implementations of `import` do not assume a `.js` file extension. As such, the relative imports used in `d3-random` all fail with a 404, because the actual files on disk _do_ have `.js` extensions, while the corresponding `import` statements in the code do not.
    
    This fixes this issue by simply adding explicit `.js` extensions to all `import` statements, thereby allowing the native browser implementations of ES Modules to successfully load these imports.

--- src/uniform.js
+++ src/uniform.js
@@ -1,4 +1,4 @@
-import defaultSource from "./defaultSource";
+import defaultSource from "./defaultSource.js";
 
 export default (function sourceRandomUniform(source) {
   function randomUniform(min, max) {
