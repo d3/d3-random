44553eb24e2e91b84efedc5e507dc5293756fc41
Philippe Rivière <fil@rezo.net>
2020-08-13 18:11:48 +0200

    updates to randomLcg

--- src/lcg.js
+++ src/lcg.js
@@ -3,6 +3,6 @@ const c = 1013904223;
 const m = 4294967296; // 2^32
 
 export default function lcg(s = 1) {
-  if (!(s >= 0)) throw new Error(“invalid seed”);
+  if (!(s >= 0)) throw new Error("invalid seed");
   return () => (s = (a * s + c) % m) / m;
 }
--- test/lcg-test.js
+++ test/lcg-test.js
@@ -5,24 +5,24 @@ require("./inDelta.js");
 
 tape("lcg is the expected deterministic PRNG", test => {
   const R1 = 0.7048832636792213;
-  var lcg = d3.randomLCG();
+  var lcg = d3.randomLcg();
   test.inDelta((lcg(), lcg(), lcg(), lcg()), R1, 1e-16);
-  lcg = d3.randomLCG();
+  lcg = d3.randomLcg();
   test.inDelta((lcg(), lcg(), lcg(), lcg()), R1, 1e-16);
   test.end();
 });
 
 tape("lcg is seeded", test => {
   const seed = 42;
-  const R42 = 0.6594497240148485;
-  const lcg = d3.randomLCG(seed);
+  const R42 = 0.22255426598712802;
+  const lcg = d3.randomLcg(seed);
   test.inDelta((lcg(), lcg(), lcg(), lcg()), R42, 1e-16);
   test.end();
 });
 
 tape("lcg is well-distributed", test => {
   const seed = 2; // 1…11 are ok
-  const lcg = d3.randomLCG(seed);
+  const lcg = d3.randomLcg(seed);
   const run = Float32Array.from({length: 10000}, lcg);
   test.inDelta(d3.mean(run), 1 / 2, 1e-2);
   test.inDelta(d3.deviation(run), Math.sqrt(1 / 12), 1e-2);
