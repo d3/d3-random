82266767f0d53105853d6a63ee26bced31ca4ee0
Philippe Rivière <fil@rezo.net>
2020-08-13 22:44:59 +0200

    accept any number as seed negative numbers will be negated; numbers between 0 and 1 inflated (follow-up on https://github.com/d3/d3-random/pull/35#discussion_r470131994)

--- src/lcg.js
+++ src/lcg.js
@@ -3,6 +3,7 @@ const c = 1013904223;
 const m = 4294967296; // 2^32
 
 export default function lcg(s = 1) {
-  if (!(s >= 0)) throw new Error("invalid seed");
+  if (s < 0) s = Math.abs(s);
+  if (s < 1) s = Math.floor(Math.pow(2, 52) * s);
   return () => (s = (a * s + c) % m) / m;
 }
