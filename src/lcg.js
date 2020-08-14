75dc4abc47ec02fc234415e60b597a92bda8ef6c
Philippe Rivière <fil@rezo.net>
2020-08-14 10:23:01 +0200

    check the profile of the first numbers for seeds 0…100

--- src/lcg.js
+++ src/lcg.js
@@ -4,6 +4,8 @@ const m = 4294967296; // 2^32
 
 export default function lcg(s = 1) {
   if (s < 0) s = Math.abs(s);
-  if (s < 1) s = Math.floor(Math.pow(2, 52) * s);
-  return () => (s = (a * s + c) % m) / m;
+  if (s < 1) s = Math.floor(a + m * s);
+  const random = () => (s = (a * s + c) % m) / m;
+  random(), random(), random(), random(); // initial mix
+  return random;
 }
--- test/lcg-test.js
+++ test/lcg-test.js
@@ -4,7 +4,7 @@ var tape = require("tape"),
 require("./inDelta.js");
 
 tape("lcg is the expected deterministic PRNG", test => {
-  const R1 = 0.7048832636792213;
+  const R1 = 0.556188570568338;
   var lcg = d3.randomLcg();
   test.inDelta((lcg(), lcg(), lcg(), lcg()), R1, 1e-16);
   lcg = d3.randomLcg();
@@ -14,7 +14,7 @@ tape("lcg is the expected deterministic PRNG", test => {
 
 tape("lcg is seeded", test => {
   const seed = 42;
-  const R42 = 0.22255426598712802;
+  const R42 = 0.1184600037522614;
   const lcg = d3.randomLcg(seed);
   test.inDelta((lcg(), lcg(), lcg(), lcg()), R42, 1e-16);
   test.end();
@@ -28,5 +28,31 @@ tape("lcg is well-distributed", test => {
   test.inDelta(d3.deviation(run), Math.sqrt(1 / 12), 1e-2);
   const histogram = d3.rollup(run, v => v.length, d => Math.floor(d * 10));
   for (const h of histogram) test.inDelta(h[1], 1000, 120);
+  test.end();
+});
+
+tape("lcg with small integer seeds is well-distributed", test => {
+  const G = d3.range(100).map(i => d3.randomLcg(i));
+  const means = [], variances = [];
+  for (let i = 0; i < 10; i++) {
+    const M = G.map(d => d());
+    means.push(d3.mean(M));
+    variances.push(d3.variance(M));
+  }
+  test.inDelta(d3.mean(means), 0.5, 0.02);
+  test.assert(d3.min(variances) > 0.75 / 12);
+  test.end();
+});
+
+tape("lcg with small fractional seeds is well-distributed", test => {
+  const G = d3.range(100).map(i => d3.randomLcg(i/100));
+  const means = [], variances = [];
+  for (let i = 0; i < 10; i++) {
+    const M = G.map(d => d());
+    means.push(d3.mean(M));
+    variances.push(d3.variance(M));
+  }
+  test.inDelta(d3.mean(means), 0.5, 0.02);
+  test.assert(d3.min(variances) > 0.75 / 12);
   test.end();
 });
