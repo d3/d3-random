ca0a54d4a628e130a7a7cbc6df5b1468d91497e7
Jacob Rus <me@jacobrus.com>
2020-08-22 02:44:07 -0700

    speed up 10x (Safari) or 4x (Chrome/Firefox)

--- src/lcg.js
+++ src/lcg.js
@@ -1,10 +1,13 @@
 // https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
-const a = 1664525;
-const c = 1013904223;
-const m = 4294967296; // 2^32
+const mul = 0x19660D;
+const inc = 0x3C6EF35F;
+const lo32 = 0xFFFFFFFF;
+const eps = Math.pow(2, -32);
 
-export default function lcg(s = Math.random()) {
-  if (!(0 <= s && s < 1)) throw new RangeError("invalid seed");
-  s = Math.floor(m * s);
-  return () => (s = (a * s + c) % m) / m;
+export default function lcg(seed = Math.random()) {
+  if (!(0 <= seed && seed < 1)) throw new RangeError("invalid seed");
+  let state = Math.floor((1+lo32) * seed);
+  return function random () {
+    return eps * ((state = (mul * state + inc) & lo32) >>> 0);
+  };
 }
