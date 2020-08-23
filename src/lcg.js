d6e3b15d1f048821372929a993f11bde2d43c7cf
Jacob Rus <me@jacobrus.com>
2020-08-22 17:09:18 -0700

    shorten code, slightly faster still

--- src/lcg.js
+++ src/lcg.js
@@ -1,13 +1,10 @@
 // https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
 const mul = 0x19660D;
 const inc = 0x3C6EF35F;
-const lo32 = 0xFFFFFFFF;
-const eps = Math.pow(2, -32);
+const eps = 1/0x100000000;
 
 export default function lcg(seed = Math.random()) {
   if (!(0 <= seed && seed < 1)) throw new RangeError("invalid seed");
-  let state = Math.floor((1+lo32) * seed);
-  return function random () {
-    return eps * ((state = (mul * state + inc) & lo32) >>> 0);
-  };
+  let state = seed/eps | 0;
+  return () => (state = mul * state + inc | 0, eps * (state >>> 0));
 }
