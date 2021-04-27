7ce414ad7325ef3ea9869ef8e490e5190e721666
Philippe Rivière <fil@rezo.net>
2021-04-27 05:05:09 +0200

    assert message

--- test/asserts.js
+++ test/asserts.js
@@ -1,10 +1,5 @@
 import assert from "assert";
 
 export function assertInDelta(actual, expected, delta) {
-  assert(expected - delta <= actual && actual <= expected + delta, {
-    message: "should be in delta",
-    operator: "inDelta",
-    actual: actual,
-    expected: [expected - delta, expected + delta]
-  });
+  assert(expected - delta <= actual && actual <= expected + delta, `${actual} should be within ${delta} of ${expected}`);
 }
